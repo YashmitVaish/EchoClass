724837f8aa5e23d59ed18a2fa3ccf2d5ec38bc77
YashmitVaish <yashmitvaish4454@gmail.com>
2025-06-24 01:07:13 +0530

    min

--- ai_layer/retrive_for_search.py
+++ ai_layer/retrive_for_search.py
@@ -8,19 +8,18 @@ dotenv.load_dotenv("ai_layer/keys.env")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# Initialize clients
 pc = Pinecone(api_key=PINECONE_API_KEY)
 embed = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=OPENAI_API_KEY)
 
 def search(text: str) -> list:
     vector = embed.embed_query(text)
 
-    index = pc.Index(index_name="your-index-name")  # Replace with your actual index name
+    index = pc.Index(index_name="rag-search-engine")  
 
     results = index.query(
         vector=vector,
         top_k=3,
-        namespace="example-namespace",  # optional
+        namespace="pdf-search",  
         include_metadata=True
     )
 
